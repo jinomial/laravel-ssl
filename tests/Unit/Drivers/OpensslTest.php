19267552cfc562d0b4ff9a05897e8e402b5a6d73
Jason Schmedes <jason@schmedes.info>
2026-04-08 18:27:30 -0700

    Fix case in OpenSsl driver tests

--- tests/Unit/Drivers/OpensslTest.php
+++ tests/Unit/Drivers/OpensslTest.php
@@ -6,7 +6,7 @@ use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\ClientInterface;
 use Jinomial\LaravelSsl\Contracts\Ssl\Driver as DriverContract;
 use Jinomial\LaravelSsl\Drivers\Driver;
-use Jinomial\LaravelSsl\Drivers\Openssl;
+use Jinomial\LaravelSsl\Drivers\OpenSsl;
 
 const HOST = 'jinomial.com';
 
@@ -14,18 +14,18 @@ uses()->group('drivers', 'openssl');
 
 it('extends Driver::class', function () {
     $client = \Mockery::mock(ClientInterface::class);
-    $driver = new Openssl('openssl', $client, 'https://example.com');
+    $driver = new OpenSsl('openssl', $client, 'https://example.com');
     expect($driver)->toBeInstanceOf(Driver::class);
 });
 
 it('is a Driver interface', function () {
-    $implementsDriver = is_a(Openssl::class, DriverContract::class, true);
+    $implementsDriver = is_a(OpenSsl::class, DriverContract::class, true);
     expect($implementsDriver)->toBeTrue();
 });
 
 it('shows a certificate', function () {
     $client = new HttpClient();
-    $driver = new Openssl('openssl', $client);
+    $driver = new OpenSsl('openssl', $client);
     $answer = $driver->show(HOST, '443');
     expect($answer->first())->toBeInstanceOf(\Jinomial\LaravelSsl\Support\Certificate::class);
     expect($answer->first()->getCommonName())->toEqual(HOST);
@@ -33,7 +33,7 @@ it('shows a certificate', function () {
 
 it('shows multiple certificates', function () {
     $client = new HttpClient();
-    $driver = new Openssl('openssl', $client);
+    $driver = new OpenSsl('openssl', $client);
     $answer = $driver->show([
         ['host' => HOST, 'port' => '110'],
         ['host' => HOST, 'port' => '443'],
